ef1b371c6c97caddfeede6f74e4718cc83f994e7
Pierre Boué <pierre.boue@le-campus-numerique.fr>
2023-01-17 15:29:06 +0100

    tables niveau 1

--- SquareGamesCreate.sql
+++ SquareGamesCreate.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS square_games;
 DROP TABLE IF EXISTS square_games.users;
 CREATE TABLE square_games.users
 (
-    id                int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    id                int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     login             varchar(255),
     password          varchar(255),
     avatar            varchar(255) DEFAULT '',
@@ -16,17 +16,29 @@ CREATE TABLE square_games.users
 -- creation pieces
 DROP TABLE IF EXISTS pieces;
 CREATE TABLE pieces (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id INTEGER(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     pattern INT(3) NOT NULL,
     picture VARCHAR(255),
     color VARCHAR(6) DEFAULT '000000');
 -- creation games
 DROP TABLE IF EXISTS games;
 CREATE TABLE games (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    id INTEGER(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name VARCHAR(32) NOT NULL ,
     age_mini_authorization INT(3) NOT NULL,
     age_maxi_authorization INT(3) NOT NULL,
     age_mini_against_human INT(3) NOT NULL,
     age_maxi_against_human INT(3) NOT NULL
+);
+-- creation boards
+DROP TABLE IF EXISTS boards;
+CREATE TABLE boards
+(
+    id INTEGER(12) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    size INT(2),
+    display_size INT(4),
+    dark_color CHAR(6) DEFAULT '333333',
+    light_color CHAR(6) DEFAULT 'DDDDDD',
+    game_id INT(10),
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE
 );
--- SquareGamesInsert.sql
+++ SquareGamesInsert.sql
@@ -8,7 +8,13 @@ VALUES
     ('boué','mdp','Piero','1967-05-14'),
     ('toto','mdp','totoegalzero','1912-12-12');
 -- INSERT PIECES
-INSERT INTO pieces (  patttern ) VALUES ( 1 ),( 2 );
+INSERT INTO square_games.pieces (  pattern ) VALUES ( 1 ),( 2 );
 -- INSERT GAMES
-INSERT INTO games( name, age_mini_authorization , age_maxi_authorization , age_mini_against_human, age_maxi_against_human )
-VALUES ('TicTacToe', 18, 99, 18, 99), ('Gomoku', 18, 99, 18, 99 ), ('Puissance 4', 18, 99, 18, 99 );
+INSERT INTO square_games.games( name, age_mini_authorization , age_maxi_authorization , age_mini_against_human, age_maxi_against_human )
+VALUES ('TicTacToe', 18, 99, 18, 99), ('Gomoku', 18, 99, 18, 99 ), ('Echecs', 18, 99, 18, 99 );
+-- Initialisation table boards
+INSERT INTO square_games.boards (size,display_size)
+VALUES
+    (3,300),
+    (12,400),
+    (10,350);
