93d914a61ffaafa72785e2a583b9c81f2aad6ebd
Pierre Boué <pierre.boue@le-campus-numerique.fr>
2023-01-17 14:37:30 +0100

    tables niveau 0

--- SquareGamesCreate.sql
+++ SquareGamesCreate.sql
@@ -1,1 +1,32 @@
----
+CREATE DATABASE IF NOT EXISTS square_games;
+-- Création table users
+DROP TABLE IF EXISTS square_games.users;
+CREATE TABLE square_games.users
+(
+    id                int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    login             varchar(255),
+    password          varchar(255),
+    avatar            varchar(255) DEFAULT '',
+    pseudo            varchar(32),
+    is_connected      tinyint DEFAULT null,
+    birthdate         DATE DEFAULT '1900-01-01',
+    id_legal_guardian int DEFAULT null,
+    coins_balance     int DEFAULT 0
+);
+-- creation pieces
+DROP TABLE IF EXISTS pieces;
+CREATE TABLE pieces (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    pattern INT(3) NOT NULL,
+    picture VARCHAR(255),
+    color VARCHAR(6) DEFAULT '000000');
+-- creation games
+DROP TABLE IF EXISTS games;
+CREATE TABLE games (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(32) NOT NULL ,
+    age_mini_authorization INT(3) NOT NULL,
+    age_maxi_authorization INT(3) NOT NULL,
+    age_mini_against_human INT(3) NOT NULL,
+    age_maxi_against_human INT(3) NOT NULL
+);
--- SquareGamesInsert.sql
+++ SquareGamesInsert.sql
@@ -1,1 +1,14 @@
---
+-- Initialisation table users
+INSERT INTO square_games.users (login, password, pseudo)
+VALUES
+    ('admin','admin','admin');
+INSERT INTO square_games.users (login, password, pseudo, birthdate)
+VALUES
+    ('chevrier','mdp','betov','1967-08-22'),
+    ('boué','mdp','Piero','1967-05-14'),
+    ('toto','mdp','totoegalzero','1912-12-12');
+-- INSERT PIECES
+INSERT INTO pieces (  patttern ) VALUES ( 1 ),( 2 );
+-- INSERT GAMES
+INSERT INTO games( name, age_mini_authorization , age_maxi_authorization , age_mini_against_human, age_maxi_against_human )
+VALUES ('TicTacToe', 18, 99, 18, 99), ('Gomoku', 18, 99, 18, 99 ), ('Puissance 4', 18, 99, 18, 99 );
