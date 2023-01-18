ff4f8e43b078989d38eac11f1c589e471d7121eb
Pierre Boué <pierre.boue@le-campus-numerique.fr>
2023-01-18 09:29:32 +0100

    Rebase

--- SquareGamesCreate.sql
+++ SquareGamesCreate.sql
@@ -16,19 +16,23 @@ CREATE TABLE square_games.users
 -- creation pieces
 DROP TABLE IF EXISTS square_games.pieces;
 CREATE TABLE square_games.pieces (
-                                     id INTEGER(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                                     pattern INT(3) NOT NULL,
-                                     picture VARCHAR(255),
-                                     color VARCHAR(6) DEFAULT '000000');
+
+    id INTEGER(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    pattern INT(3) NOT NULL,
+    picture VARCHAR(255),
+    color VARCHAR(6) DEFAULT '000000');
 -- creation games
 DROP TABLE IF EXISTS square_games.games;
 CREATE TABLE square_games.games (
-                                    id INTEGER(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-                                    name VARCHAR(32) NOT NULL ,
-                                    age_mini_authorization INT(3) NOT NULL,
-                                    age_maxi_authorization INT(3) NOT NULL,
-                                    age_mini_against_human INT(3) NOT NULL,
-                                    age_maxi_against_human INT(3) NOT NULL
+    id INTEGER(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(32) NOT NULL ,
+    age_mini_authorization INT(3) NOT NULL,
+    age_maxi_authorization INT(3) NOT NULL,
+    age_mini_against_human INT(3) NOT NULL,
+    age_maxi_against_human INT(3) NOT NULL,
+    default_board_id INT(10),
+    FOREIGN KEY ( default_board_id ) REFERENCES boards(id) ON DELETE CASCADE
+
 );
 -- creation boards
 DROP TABLE IF EXISTS square_games.boards;
